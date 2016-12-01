58c0908fc7967f15a2d40c6205e6b7250fc3f1c3
tcr <tcr@ableton.com>
2016-12-01 09:04:22 +0100

    use system env user home in templates directory definition

--- app/patron.js
+++ app/patron.js
@@ -5,9 +5,10 @@ const helpers = require('./helpers.js');
 exports.defaultTemplatesDir = function() {
   var platform = os.platform()
   var arch = os.arch()
+  var user_home = helpers.getUserHome();
 
   if (platform == 'win32') {
-    return 'C:\\Users\\oleia\\Documents\\Ableton\\User\ Templates';
+    return `${user_home}\\Documents\\Ableton\\User\ Templates`;
   } else if (platform == 'darwin') {
     var user_home = helpers.getUserHome();
     return `${user_home}/Music/Ableton/Templates`;
